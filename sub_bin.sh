c12ee1a893f710934ce1f859abf98d393e464828
f_tash <ftashtashtash@outlook.com>
2024-08-29 19:10:48 +0900

    fix submit via binary

--- sub_bin.sh
+++ sub_bin.sh
@@ -17,7 +17,7 @@ destination_file="binary/main/src/main.rs"
 cp $original $destination_file
 
 cd binary/main
-RUSTFLAGS="-C linker=x86_64-linux-musl-gcc" cargo +nightly build --target x86_64-unknown-linux-musl --release -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort  > output.txt 2>&1
+RUSTFLAGS="-C linker=x86_64-linux-musl-gcc" cargo +nightly build --target x86_64-unknown-linux-musl --release -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort  >/dev/null  2>&1
 
 
 base64 -i target/x86_64-unknown-linux-musl/release/main | tr -d '\n' > encoded_binary.txt
